2e9afe7f613523775ebdac65b3d98b81c48e0c78
yuttana chuay <yutanaofficial@gmail.com>
2024-09-06 10:10:46 +0700

    feat: Update register form and index views with new fields and styling

--- app/controllers/registers_controller.rb
+++ app/controllers/registers_controller.rb
@@ -1,18 +1,46 @@
 class RegistersController < ApplicationController
+  before_action :set_register, only: [ :edit, :update, :destroy ]
+
   def index
-    @register = Register.all
+    @registers = Register.all
   end
+
   def new
     @register = Register.new
   end
+
   def create
     @register = Register.new(register_params)
     if @register.save
-      redirect_to root_path
+      redirect_to registers_path, notice: "Registration successful!"
     else
-      render "new"
+      render :new
     end
   end
+
+  def edit
+    # Uses the @register from before_action :set_register
+  end
+
+  def update
+    if @register.update(register_params)
+      redirect_to registers_path, notice: "Record updated successfully!"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @register.destroy
+    redirect_to registers_path, notice: "Record deleted successfully!"
+  end
+
+  private
+
+  def set_register
+    @register = Register.find(params[:id])
+  end
+
   def register_params
     params.require(:register).permit(:fistName, :lastName, :birthDay, :gender, :email, :phoneNumber, :subJect)
   end
